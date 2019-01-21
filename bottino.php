0236b4114f717d0b2d35a2db01543652d38dd250
giovanniacanforah <schizzapple@gmail.com>
2019-01-21 18:28:11 +0100

    Update bottino.php
    Vuoi creare il tuo bot?
    Vai in @Manybot

--- bottino.php
+++ bottino.php
@@ -2,11 +2,11 @@
 if(!file_exists("already")){
     if($argv[1] == "install"){
       file_put_contents("already", "installed");  
-      echo 'Okay, installed.'.PHP_EOL;
+      echo 'Okay, installed.';
       echo PHP_EOL.'Start @ManagerFatherBot on Telegram for continue!'.PHP_EOL;
     }else echo 'Do "php bottino.php install" for install'.PHP_EOL;
 }else echo 'Start @managerfatherbot on Telegram for continue.'.PHP_EOL;
 
 
-#IRONIC CODE BY @CAGATEMI ON TELEGRAM.
-#IF YOU WANT TO USE BOTTINO'S FEATURE, ADD TO A TELEGRAM GROUP @ManagerFatherBot.
+/*IRONIC CODE BY @CAGATEMI ON TELEGRAM.
+IF YOU WANT TO USE BOTTINO'S FEATURE, ADD TO A TELEGRAM GROUP @ManagerFatherBot.*/
