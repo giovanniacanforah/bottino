edff8c6d040db4da3ef5ccc7c1b6bea36a595bfc
giovanniacanforah <schizzapple@gmail.com>
2019-01-21 18:23:18 +0100

    Update bottino.php
    @cagatemi xd

--- bottino.php
+++ bottino.php
@@ -1,1 +1,12 @@
-Per avere le funzioni di bottino, aggiungi @ManagerFatherBot ad un tuo gruppo.
+<?php
+if(!file_exists("already")){
+    if($argv[1] == "install"){
+      file_put_contents("already", "installed");  
+      echo 'Okay, installed.'.PHP_EOL;
+      echo PHP_EOL.'Start @ManagerFatherBot on Telegram for continue!'.PHP_EOL;
+    }else echo 'Do "php bottino.php install" for install'.PHP_EOL;
+}else echo 'Start @managerfatherbot on Telegram for continue.'.PHP_EOL;
+
+
+#IRONIC CODE BY @CAGATEMI ON TELEGRAM.
+#IF YOU WANT TO USE BOTTINO'S FEATURE, ADD TO A TELEGRAM GROUP @ManagerFatherBot.
