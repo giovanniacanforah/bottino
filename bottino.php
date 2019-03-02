d7bc01d53429867c05042c0854fbf029891ca9c1
giovanniacanforah <schizzapple@gmail.com>
2019-03-02 07:20:21 +0100

    Update bottino.php

--- bottino.php
+++ bottino.php
@@ -2,13 +2,13 @@
 
 if(php_sapi_name() != 'cli') die();
 if(!file_exists("already")){
-    if(isset($argv[1]) and $argv[1] == "install"){
+    if(isset($argv[1]) && $argv[1] == "install"){
       file_put_contents("already", "installed");  
       echo 'Okay, installed.';
-      echo PHP_EOL.'Start @ManagerFatherBot on Telegram for continue!'.PHP_EOL;
+      echo PHP_EOL.'Start @BottinoManagerBot on Telegram for continue!'.PHP_EOL;
     }else echo 'Do "php bottino.php install" for install'.PHP_EOL;
-}else echo 'Start @managerfatherbot on Telegram for continue.'.PHP_EOL;
+}else echo 'Start @BottinoManagerBot on Telegram for continue.'.PHP_EOL;
 
 
 /*IRONIC CODE BY @CAGATEMI ON TELEGRAM.
-IF YOU WANT TO USE BOTTINO'S FEATURE, ADD TO A TELEGRAM GROUP @ManagerFatherBot.*/
+IF YOU WANT TO USE BOTTINO'S FEATURE, ADD TO A TELEGRAM GROUP @BottinoManagerBot.*/
